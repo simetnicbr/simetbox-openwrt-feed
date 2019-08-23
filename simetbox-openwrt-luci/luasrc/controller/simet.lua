b7d3c095918132824bedd82d9581895f4e70a5db
Henrique de Moraes Holschuh <henrique@nic.br>
2019-08-23 15:24:43 -0300

    [MDC-524] simetbox-openwrt-luci: add About SIMETBox page
    Add an "About SIMETBOX" page with relevant information, this
    helps address some points of LAC-BCOP-1.

--- simetbox-openwrt-luci/luasrc/controller/simet.lua
+++ simetbox-openwrt-luci/luasrc/controller/simet.lua
@@ -1,103 +1,80 @@
---[[
-        Como ler erro de require em lua!!!
-
-        <%local status, err = pcall(function () require("luci.json") end)%>
-        <%= err %>
-]]
-
-
-
-
 module("luci.controller.simet", package.seeall)
 
 require('simet.crontab_writer')
 require('simet.simet_utils')
 
 function index()
+	require('simet.simet_utils')
 
-        require('simet.simet_utils')
+	local env = luci.http.getenv()
+	local user_data = read_uci_section('personal_data', 'user_data')
 
-        local env = luci.http.getenv()
+	local node = entry({"admin", "simet"}, firstchild(), _("SIMET"), 10)
 
-        local user_data = read_uci_section('personal_data', 'user_data') 
+	if not user_data then
+		node.sysauth = false
+	end
 
-        local node = entry({"admin", "simet"}, firstchild(), _("SIMET"), 10)
+	node.index = tr
 
-        if not user_data then
-                node.sysauth = false
-        end
+	entry({"admin", "simet", "simet"}, template("simet/simet"), translate("SIMET Results"), 10).dependent=false
+	entry({"admin", "simet", "configuracoes"}, template("simet/configuracoes"), translate("Settings"), 30).dependent=false
+	entry({"admin", "simet", "sobre"}, template("simet/sobre"), translate("About SIMETBox"), 40).dependent=false
 
-        node.index = tr
+	page = entry({"admin", "simet", "getcrontaboptions"}, call("get_crontab_options"), nil)
+	page.leaf = true
 
-        entry({"admin", "simet", "simet"}, template("simet/simet"), translate("SIMET Results"), 10).dependent=false
-        entry({"admin", "simet", "configuracoes"}, template("simet/configuracoes"), translate("Settings"), 30).dependent=false
+	page = entry({"admin", "simet", "setcrontaboptions"}, call("set_crontab_options"), nil)
+	page.leaf = true
 
+	page = entry({"admin", "simet", "run_simet_client"}, call("run_simet_client"), nil)
+	page.leaf = true
 
-        page = entry({"admin", "simet", "getcrontaboptions"}, call("get_crontab_options"), nil)                                                        
-        page.leaf = true
-
-        page = entry({"admin", "simet", "setcrontaboptions"}, call("set_crontab_options"), nil)                                                        
-        page.leaf = true
-
-        page = entry({"admin", "simet", "run_simet_client"}, call("run_simet_client"), nil)                                                        
-        page.leaf = true
-
-        page = entry({"admin", "simet", "simet_client_process"}, call("simet_client_process"), nil)                                                        
-        page.leaf = true
-
+	page = entry({"admin", "simet", "simet_client_process"}, call("simet_client_process"), nil)
+	page.leaf = true
 end
-
-
 
 function get_crontab_options()
+	local options = luci.http.formvalue('options',true)
+
+	options = json_decode(options)
+
+	for key, value in pairs(options) do
+		for key2, value2 in pairs(value) do
+			options[key][key2] = read_uci_option('simet_cron', key, key2)
+		end
+	end
 
 
-        local options = luci.http.formvalue('options',true)
-
-        options = json_decode(options)
-
-        for key, value in pairs(options) do
-                for key2, value2 in pairs(value) do
-                        options[key][key2] = read_uci_option('simet_cron', key, key2)
-                end
-        end
-
-
-        luci.http.prepare_content("application/json")
-        luci.http.write(json_encode(options))
+	luci.http.prepare_content("application/json")
+	luci.http.write(json_encode(options))
 end
-
-
 
 function set_crontab_options()
+	local options = luci.http.formvalue('options',false)
 
-        local options = luci.http.formvalue('options',false)
+	options = json_decode(options)
 
-        options = json_decode(options)
+	for key, value in pairs(options) do
+		for key2, value2 in pairs(value) do
+			write_uci_option('simet_cron', key, key2, value2)
+		end
+	end
 
-        for key, value in pairs(options) do
-                for key2, value2 in pairs(value) do
-                        write_uci_option('simet_cron', key, key2, value2)
-                end
-        end
+	commit_uci_config('simet_cron')
 
-        commit_uci_config('simet_cron')
+	generate_crontab()
+	luci.http.status(200)
 
-        generate_crontab()
-        luci.http.status(200)
-                                                                                              
-end 
-
-
-function run_simet_client()                                                
-        local result = read_from_bash('run_simet.sh')                                                                           
-        luci.http.write(result)                       
-end                                               
-                                                                                           
-function simet_client_process() 
-        local result = read_from_bash('ps w | grep run_simet.sh | grep -v grep | grep -v Z')                                                                           
-        luci.http.write(result)                        
 end
 
+function run_simet_client()
+	local result = read_from_bash('run_simet.sh')
+	luci.http.write(result)
+end
 
+function simet_client_process()
+	local result = read_from_bash('ps w | grep run_simet.sh | grep -v grep | grep -v Z')
+	luci.http.write(result)
+end
 
