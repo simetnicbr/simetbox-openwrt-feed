35fea339b3005265c6661e001d10c199fd9855c9
Henrique de Moraes Holschuh <henrique@nic.br>
2022-11-25 09:29:17 -0300

    [MDC-1309] lmapd: try harder to restart
    * Ignore dangling symlinks in /etc/init.d/simet-lmapd::init_lmapd_fs
    
    * Ask procd to wait 60s between restarts of lmapd, tolerate 10 restarts
      every 30 minutes before giving up
    
    * Ask procd to *start* (not restart!) the simet-lmapd service daily, it
      does the right thing (i.e. nothing!) if the service is already
      running, but if it is not, it clears procd error-tracking conditions
      (messages, respawn timers) and starts it.
    
    * Checks daily for a zero-sized or unreadable schedule, and force-reload
      (or creates) a new one in that case.  Due to possible races, the
      simet-lmapd initscript does not try to replace an unreadable or
      zero-sized schedule with the emergency schedule at the moment.

--- simetbox-openwrt-simet-ma/files/usr/bin/simetbox_refresh_cron.sh
+++ simetbox-openwrt-simet-ma/files/usr/bin/simetbox_refresh_cron.sh
@@ -8,11 +8,22 @@ set +e
 # force-renew registration
 simet_register_ma.sh >/dev/null
 
-# force-reload schedule when too old (lmapd might have crashed)
-. /usr/lib/simet/simet_lib_config.sh \
-	&& test -n "$(find $LMAP_SCHEDULE_FILE -mtime +1)" \
-	&& simetbox_lmap-fetch-schedule.sh || true
+(
+  . /usr/lib/simet/simet_lib_config.sh && {
+    # force-reload schedule if it is unreadable for whatever reason
+    # as this might preclude the activation of the emergency schedule
+    test -s "$LMAP_SCHEDULE_FILE" || simetbox_lmap-fetch-schedule.sh
+
+    # force-reload schedule when too old (lmapd might have crashed)
+    test -n "$(find "$LMAP_SCHEDULE_FILE" -mtime +1)" \
+      && simetbox_lmap-fetch-schedule.sh
+  }
+)
 
 # self-heal permissions
-. /etc/init.d/simet-lmapd && init_lmapd_fs
+( . /etc/init.d/simet-lmapd && init_lmapd_fs )
+
+# start lmapd if it was stopped.  Does the right things under procd
+# FIXME: don't do this if the measurement engine is disabled
+/etc/init.d/simet-lmapd start
 :
