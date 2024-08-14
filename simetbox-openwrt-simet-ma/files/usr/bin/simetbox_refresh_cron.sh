9ab447e91c8243e38d250c569aa35d2397a66b06
Henrique de Moraes Holschuh <henrique@nic.br>
2024-08-14 15:31:44 -0300

    [MDC-1463][MDC-1466] simetbox-openwrt-simet-ma,lmapd: update versions
    simetbox-openwrt-simet-lmapd: 0.15.5-2:
    
    * Remove the main schedule from the builtin lmapd compile-time path,
      as required by the simet_lib_lmap API and simet-ma 0.18.7+
    * initscript: partially switch to the multi-instance simet_lib_lmap.sh
      API.  Support only the main instance for now, keep most paths
      hardcoded.
    * initscript: adjust permissions of /etc/simet and /etc/simet/lmap
      on start, if they deviate from the expected.  For reload, adjust
      only /etc/simet/lmap/*.json
    
    Requires simet-ma 0.18.7+.
    
    simetbox-openwrt-simet-ma: 0.18.11-1:
    
    * Update Config help text for SIMET2_SPOOFER (no longer experimental,
      doesn't require openssl), add version of OpenWRT Chaos Calmer to
      the help text for SIMETBOX_APPLINK.
    * Improve postinst to ensure there's a main schedule in place, and
      that the AGENT_LOCK is reset
    * sspooferc needs to be SUID root (required to avoid the mess with
      filesystem capability support.  It just needs CAP_NET_RAW).
      chmod g+s it using an uci-defaults script.
    * Switch simetbox_refresh_cron to the simet_lib_lmapd API.
    * rpcd, init.d: run simet_register-ma.sh and
      simetbox_lmap-fetch-schedule.sh as user nicbr-simet
    * simet_register-ma hooks: do not attempt to use the initscript to
      reload simet-ma, send SIGHUP to inetupc directly.
    * hotplug.d/iface: reloads/starts simet-ma, starts simet-lmapd
      (can't reload simet-lmapd, it would be disruptive)
    * cron.daily: start simet-ma as well, not just simet-lmapd
    * Remove uci simet_ma.ixbr_ping config entries, that measurement
      does not exist anymore
    * Remove ixbr_ping entries from ubus rpcd script

--- simetbox-openwrt-simet-ma/files/usr/bin/simetbox_refresh_cron.sh
+++ simetbox-openwrt-simet-ma/files/usr/bin/simetbox_refresh_cron.sh
@@ -5,25 +5,34 @@
 
 set +e
 
+# self-heal permissions
+( . /etc/init.d/simet-ma && init_simetma_fs )
+( . /etc/init.d/simet-lmapd && init_lmapd_fs )
+
 # force-renew registration
-simet_register_ma.sh >/dev/null
+start-stop-daemon -S -c nicbr-simet:nicbr-simet -n simet_reg_ma \
+                  -x /usr/bin/simet_register_ma.sh >/dev/null
 
 (
-  . /usr/lib/simet/simet_lib_config.sh && {
+  . /usr/lib/simet/simet_lib_config.sh && \
+  . /usr/lib/simet/simet_lib_lmapd.sh && \
+  LMAP_SCHEDULE_FILE=$(lmapd_get_sched_filename main) && \
+  [ -n "$LMAP_SCHEDULE_FILE" ] && {
     # force-reload schedule if it is unreadable for whatever reason
     # as this might preclude the activation of the emergency schedule
-    test -s "$LMAP_SCHEDULE_FILE" || simetbox_lmap-fetch-schedule.sh
-
-    # force-reload schedule when too old (lmapd might have crashed)
-    test -n "$(find "$LMAP_SCHEDULE_FILE" -mtime +1)" \
-      && simetbox_lmap-fetch-schedule.sh
+    # also force-reload schedule when too old (lmapd might have crashed)
+    DOIT=
+    [ -s "$LMAP_SCHEDULE_FILE" ] || DOIT=1
+    [ -z "$DOIT" ] && test -n "$(find "$LMAP_SCHEDULE_FILE" -type f -mtime +1)" && DOIT=1
+    [ -n "$DOIT" ] && {
+      start-stop-daemon -S -c nicbr-simet:nicbr-simet -n simet_fetch_sched \
+                        -x simetbox_lmap-fetch-schedule.sh
+    }
   }
 )
 
-# self-heal permissions
-( . /etc/init.d/simet-lmapd && init_lmapd_fs )
-
-# start lmapd if it was stopped.  Does the right things under procd
+# start simet-ma and lmapd if they were stopped.  Does the right things under procd
 # FIXME: don't do this if the measurement engine is disabled
-/etc/init.d/simet-lmapd start
+/etc/init.d/simet-ma start || true
+/etc/init.d/simet-lmapd start || true
 :
